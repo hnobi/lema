d98f79222dc3dbbaf9e42c45d598eb578849fad9
hnobi <hnobi08@yahoo.com>
2025-07-24 14:58:15 +0100

    add default route

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
 app.use('/api/users', usersRoutes);
 app.use('/api/posts', postsRoutes);
 
